ef59c0888948fc655b38a6a0e08e679906b4b738
Robert Ward <robsward@gmail.com>
2025-03-17 19:55:29 +0100

    dumping some stuff in the scrapyard file

---
+++ scrapyard.rs
@@ -0,0 +1,113 @@
+fn thing() {
+    // Get user query.
+    println!("Welcome to CLIraoke.");
+    print!("Please type a song or artist name: ");
+    let query_base = cli::cli::get_user_input();
+
+    // TODO: Run lyric and video fetching concurrently.
+    // Show options for the query:
+    let videos = audio::audio::fetch_videos(yt_api_key, query_base.as_str()).await;
+
+    if videos.is_err() {
+        println!(
+            "Error fetching videos: {}",
+            videos.err().and_then(|e| Some(e.to_string())).unwrap()
+        );
+        exit(1);
+    }
+
+    let videos = videos?; // Something about borrow checker? who knows
+    if videos.is_empty() {
+        println!("No videos found");
+        exit(1);
+    }
+
+    // Get the video id from the user:
+    let mut vid_opts: Vec<CLIOption> = Vec::new();
+    for video in videos {
+        let opt = CLIOption {
+            artist: Some(video.artist),
+            id: video.id,
+            title: video.title,
+        };
+        vid_opts.push(opt);
+    }
+
+    let video_opt =
+        tokio::task::spawn_blocking(move || cli::cli::present_options(vid_opts)).await?;
+
+    // Get the lyrics for the query:
+    let lyrs = lyrics::lyrics::search_lyrics(query_base.as_str()).await?;
+    // if lyrs.is_err() {
+    //     println!("We found your song, but had a problem finding the lyrics: {}", lyrs.err().unwrap());
+    //     exit(1);
+    // }
+
+    if lyrs.is_empty() {
+        println!("No lyrics found for this song.");
+        exit(1);
+    }
+
+    println!("What a banger. OK, now select the lyrics to use: ");
+
+    // Show options for the lyrics:
+    let mut lyr_opts: Vec<CLIOption> = Vec::new();
+    for lyr in lyrs {
+        let opt = CLIOption {
+            artist: Some(lyr.artist),
+            id: lyr.id,
+            title: lyr.title,
+        };
+        lyr_opts.push(opt);
+    }
+
+    let lyric_opt =
+        tokio::task::spawn_blocking(move || cli::cli::present_options(lyr_opts)).await?;
+
+    let lyrics_thread = tokio::task::spawn_blocking(move || {
+        if let Some(opt) = lyric_opt {
+            lyrics::lyrics::fetch_lyrics(opt.id.as_str())
+        } else {
+            None
+        }
+    });
+
+    // Get the audio URL for the video:
+    let mut audio_url: Option<String> = None;
+    if let Some(video_opt) = video_opt {
+        audio_url = tokio::task::spawn_blocking(move || {
+            audio::audio::get_youtube_audio_url(video_opt.id.as_str())
+        })
+            .await?;
+    }
+
+    if audio_url.is_none() {
+        println!("Error getting audio URL");
+        exit(1);
+    }
+
+    // Wait for the lyrics to be ready before playing.
+    match lyrics_thread.await {
+        Ok(Some(lyrics_map)) => {
+            let _play_thread = std::thread::spawn(move || {
+                audio::audio::play_audio(audio_url.unwrap().as_str());
+            });
+
+            // Display lyrics in sync with playback
+            lyrics::lyrics::display_synced_lyrics(&lyrics_map);
+
+            // Wait for audio playback to finish
+            if let Err(e) = tokio::signal::ctrl_c().await {
+                println!("Error waiting for Ctrl+C: {}", e);
+            }
+
+            // Audio playback has stopped
+            println!("Playback ended");
+        }
+        _ => {
+            println!("No lyrics available for this track.");
+        }
+    }
+
+    Ok(())
+}
